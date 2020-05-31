f0364565f364522a62b2a83503be31b6e39ad6e8
hoai-nguyen <nguyenvanhoai.cs@gmail.com>
2020-05-31 16:44:37 +0900

    init repo

---
+++ js/vendor/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_OzHpesi4N',
+        userPoolClientId: '1753cv6gkbd5h0uj600hpphon7',
+        region: 'ap-southeast-1'
+    },
+};
